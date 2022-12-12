646f08eb24050ad438990216fa7918ed9fb29eb0
Kenji Lao <lao.kenjii@gmail.com>
2022-12-12 15:42:03 +0100

    Exo tableaux

--- CSharp 12 12/Program.cs
+++ CSharp 12 12/Program.cs
@@ -10,6 +10,47 @@ namespace CSharp_12_12
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello world");
+
+            short s = 300;
+            byte q = (byte)s;
+            Console.WriteLine(q);
+
+
+            LesJoursDeLaSemaine weekEnd = LesJoursDeLaSemaine.Samedi | LesJoursDeLaSemaine.Dimanche;
+            Console.WriteLine("Jours : " + weekEnd);
+
+            int[][] Tableau = new int[3][];
+            Tableau[0] = new int[] { 42, 2 , 56};
+            Tableau[1] = new int[] { 54, 25 , 1 , 35 , 56};
+
+
+
+            //Tentez de parcourir des tableaux multidimensionnels avec des boucles for
+            for (int index = 0; index < Tableau.Length-1; index++) {
+                for (int index1 = 0; index1 < Tableau[index].Length; index1++)
+                {
+                    Console.WriteLine("Parcours du tableau multidimensionnel" + " : " + Tableau[index][index1]);//Afficher tous les éléments de Tableau
+                }
+            }
+            
+
+
+            Console.ReadLine();
+
+            Console.ReadKey();
         }
+
+        enum LesJoursDeLaSemaine
+        {
+            Lundi,
+            Mardi,
+            Mercredi,
+            Jeudi,
+            Vendredi,
+            Samedi,
+            Dimanche
+        }
+
     }
 }
